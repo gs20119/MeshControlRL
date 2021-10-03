8d866e49de695cc6a400dd5473ff6881ef3443ab
gs20119 <andrew04247@gmail.com>
2021-10-03 12:59:18 +0900

    Fixed Action, State, Reward Structure in Vanila TD3 Lab

---
+++ Day 12/TD3/main.py
@@ -0,0 +1,102 @@
+
+import sys
+import math
+from simulation import *
+from td3 import *
+from collections import deque
+import matplotlib.pyplot as plt
+
+o, X = 1, -1
+bonus = [
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+]
+
+N, M, L, K, aK, sK = 16, 1 / 16, 100, 7, 90000, 0.0
+DAMP, aDAMP = 0.3, 0.3
+mvcN, mvcR = 50, 0.7 * L
+HIDEEXTRA = False  # 여기에서 제어점 표시 여부 결정
+
+pygame.init()
+screen = pygame.display.set_mode([800, 600])
+pygame.display.set_caption("Simulation")
+clock = pygame.time.Clock()
+clock.tick(FPS)
+simulation = Simulation(N, M, L, K, aK, sK, DAMP, aDAMP, bonus, mvcN, mvcR, screen)
+done = False
+
+train_mode = True
+load_model = False
+
+num_epochs = 50000
+max_step = 500
+discount_factor = 0.99
+step = 0
+episode = 0
+start_train_episode = 10
+print_interval = 10
+actor_loss = None
+critic_loss = None
+
+state_size = simulation.state_size()
+action_size = simulation.action_size()
+
+agent = TD3(state_size, action_size)
+rewards = deque(maxlen=print_interval)
+
+
+state = simulation.reset()
+done = False
+step = 0
+
+for episode in range(num_epochs) :
+    step =0
+    done = False
+    state = simulation.reset()
+
+    while step<max_step and (done==False):
+        step += 1
+        action = agent.get_action([state])
+        #print(action)
+        next_state, reward, done = simulation.step(action)
+
+        if train_mode:
+            agent.append_sample(state, action, reward, next_state, done)
+
+        state = next_state
+        if train_mode and episode >= start_train_episode:
+            actor_loss, critic_loss = agent.train()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        simulation.render(HIDEEXTRA)
+        pygame.display.flip()
+
+        if step % print_interval == 0:
+            print("episode : {} / step: {} / actor_loss: {} / critic_loss: {} / value: {}".format(episode, step, actor_loss, critic_loss, reward))
+            #print("main action : {}, {}".format(action[2*N],action[2*N+1]))
+
---
+++ Day 12/TD3/utils.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+import math
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def convertToTensor(input, input_size, batsize=1):
+    input = input.reshape(1,-1) 
+    return torch.FloatTensor(input).to(device)
+
+def calculate_log_pi(log_stds, noises, actions):
+    gaussian_log_probs = (-0.5 * noises.pow(2) - log_stds).sum(dim=-1, keepdim=True) - 0.5 * math.log(2 * math.pi) * log_stds.size(-1)
+
+    log_pis = gaussian_log_probs - torch.log(1 - actions.pow(2) + 1e-6).sum(dim=-1, keepdim=True)
+
+    return log_pis
+
+def reparameterize(means, log_stds):
+    stds = log_stds.exp()
+    noises = torch.randn_like(means)
+    us = means + noises * stds
+    actions = torch.tanh(us)
+    log_pis = calculate_log_pi(log_stds, noises, actions)
+
+    return actions, log_pis
