1bc5d664c491b00c41960fe2a1e4d233659f77ed
gs20119 <andrew04247@gmail.com>
2021-10-27 13:58:43 +0900

    Changed State to image and used CNN

---
+++ Day 12/CNN+TD3/main.py
@@ -0,0 +1,126 @@
+
+import sys
+import math
+from simulation import *
+from td3 import *
+from collections import deque
+import matplotlib.pyplot as plt
+
+o, X = 1, -1
+bonus = [
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, o, o, o, o, o, o, o, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+[X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
+]
+
+N, M, L, K, aK, sK = 32, 1/32, 100, 7.5, 90000, 0.0
+DAMP, aDAMP = 0.2, 0.3
+mvcN, mvcR = 50, 0.7 * L
+HIDEEXTRA = True  # 여기에서 제어점 표시 여부 결정
+HIDEBOARD = False
+
+pygame.init()
+screen = pygame.display.set_mode([800, 600])
+pygame.display.set_caption("Simulation")
+clock = pygame.time.Clock()
+clock.tick(FPS)
+simulation = Simulation(N, M, L, K, aK, sK, DAMP, aDAMP, bonus, mvcN, mvcR, screen)
+done = False
+
+train_mode = True
+load_model = False
+
+num_epochs = 50000
+max_step = 500
+discount_factor = 0.99
+step = 0
+episode = 0
+start_train_episode = 10
+print_interval = 10
+actor_loss = None
+critic_loss = None
+
+state_size = simulation.state_size()
+action_size = simulation.action_size()
+
+agent = TD3(state_size, action_size)
+
+state = simulation.reset()
+done = False
+ended = False
+step = 0
+critic_losses = deque(maxlen=100)
+actor_losses = deque(maxlen=100)
+critic_average = []
+actor_average = []
+sum_actor = 0
+sum_critic = 0
+
+for episode in range(num_epochs):
+    step = 0
+    done = False
+    state = simulation.reset()
+
+    while step<max_step and (done==False):
+        step += 1
+        action = agent.get_action([state])
+        #print(action)
+        next_state, reward, done = simulation.step(action)
+
+        if train_mode:
+            agent.append_sample(state, action, reward, next_state, done)
+
+        state = next_state
+        if train_mode and episode >= start_train_episode:
+            actor_loss, critic_loss = agent.train()
+            actor_losses.append(actor_loss)
+            critic_losses.append(critic_loss)
+            sum_actor += actor_loss
+            sum_critic += critic_loss
+            if len(critic_losses) == 100:
+                sum_actor -= actor_losses[0]
+                sum_critic -= critic_losses[0]
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                ended=True
+
+        simulation.render(HIDEEXTRA, HIDEBOARD)
+        pygame.display.flip()
+
+        if step % print_interval == 0:
+            print("episode : {} / step: {} / actor_loss: {} / critic_loss: {} / value: {}".format(episode, step, actor_loss, critic_loss, reward))
+            #print("main action : {}, {}".format(action[2*N],action[2*N+1]))   
+
+    actor_average.append(sum_actor/100)
+    critic_average.append(sum_critic/100) 
+
+    if ended:
+        break
+
+plt.figure()
+plt.plot(actor_average)
+plt.figure()
+plt.plot(critic_average)
+plt.show()
---
+++ Day 12/CNN+TD3/td3.py
@@ -0,0 +1,165 @@
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+import random
+from utils import *
+from collections import deque
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Actor, self).__init__()
+
+        self.layer1 = nn.Linear(state_dim, 256)
+        self.layer2 = nn.Linear(256, 256)
+        self.layer3 = nn.Linear(256, action_dim)
+
+    def forward(self, state):
+        a = F.relu(self.layer1(state))
+        a = F.relu(self.layer2(a))
+        return torch.tanh(self.layer3(a))
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+        self.layer1 = nn.Linear(state_dim + action_dim, 256)
+        self.layer2 = nn.Linear(256, 256)
+        self.layer3 = nn.Linear(256, 1)
+
+        self.layer4 = nn.Linear(state_dim + action_dim, 256)
+        self.layer5 = nn.Linear(256, 256)
+        self.layer6 = nn.Linear(256, 1)
+
+    def forward(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.layer1(sa))
+        q1 = F.relu(self.layer2(q1))
+        q1 = self.layer3(q1)
+
+        q2 = F.relu(self.layer4(sa))
+        q2 = F.relu(self.layer5(q2))
+        q2 = self.layer6(q2)
+        return q1, q2
+
+    def Q(self, state, action):
+        sa = torch.cat([state, action], 1)
+        q = F.relu(self.layer1(sa))
+        q = F.relu(self.layer2(q))
+        q = self.layer3(q)
+        return q
+
+
+class TD3(object):
+    def __init__(
+        self, state_dim, action_dim, discount=0.99, explore = 0.0,
+        tau=0.005, actor_noise=0.2, noise_clip=0.5, delay=2, bufferSize=50000
+    ):
+        self.actor = Actor(state_dim, action_dim).to(device)
+        self.actor_target = copy.deepcopy(self.actor)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = copy.deepcopy(self.critic)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+        
+        self.discount = discount
+        self.tau = tau
+        self.noise = actor_noise
+        self.explore = explore
+        self.noise_clip = noise_clip
+        self.delay = delay
+
+        self.replay = deque(maxlen = bufferSize)
+        self.iteration = 0
+
+
+    def get_action(self, state):
+        state = torch.FloatTensor(np.reshape(state,(1,-1))).to(device) 
+        action = self.actor(state).cpu().data.numpy().flatten()
+        action += np.random.randn(len(action)) * self.explore
+        return action
+
+
+    def append_sample(self, state, action, rewards, next_state, done):
+        self.replay.append((state, action, rewards, next_state, done))
+
+
+    def sample(self, batch_size):
+        batch = random.sample(self.replay, batch_size)
+        return(
+            torch.FloatTensor([sample[0] for sample in batch]).to(device),
+            torch.FloatTensor([sample[1] for sample in batch]).to(device),
+            torch.FloatTensor([sample[2] for sample in batch]).to(device),
+            torch.FloatTensor([sample[3] for sample in batch]).to(device),
+            torch.FloatTensor([sample[4] for sample in batch]).to(device)
+        )
+
+
+    def train(self, batch_size=128):
+        self.iteration += 1
+
+        # Sample Replay Buffer
+        state, action, reward, next_state, done = self.sample(batch_size)
+
+        with torch.no_grad():
+            noise = (
+                torch.randn_like(action)*self.noise
+            ).clamp(-self.noise_clip, self.noise_clip)
+            next_action = (
+                self.actor_target(next_state) + noise
+            ).clamp(-1,1)
+
+        # Compute Target Q & current Q estimate
+        target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+        target_Q = torch.min(target_Q1, target_Q2)
+        target_Q = reward + (1-done) * self.discount * target_Q
+        current_Q1, current_Q2 = self.critic(state, action)
+
+        # Compute critic loss and Optimize
+        critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # Compute actor loss and Optimize with delay
+        actor_loss = -self.critic.Q(state, self.actor(state)).mean()
+
+        if self.iteration % self.delay == 0:    
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            self.soft_update_target(self.critic_target, self.critic)
+            self.soft_update_target(self.actor_target, self.actor)
+
+        return actor_loss.detach().cpu().numpy(), critic_loss.detach().cpu().numpy()
+
+
+    def soft_update_target(self, target, origin):
+        for target_param, param in zip(target.parameters(), origin.parameters()):
+            target_param.data.copy_((1-self.tau) * target_param.data + self.tau * param.data)
+
+
+    def save(self, filename):
+        torch.save(self.critic.state_dict(), filename + "_critic")
+        torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
+        torch.save(self.actor.state_dict(), filename + "_actor")
+        torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
+
+
+    def load(self, filename):
+	    self.critic.load_state_dict(torch.load(filename + "_critic"))
+	    self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer"))
+	    self.critic_target = copy.deepcopy(self.critic)
+
+	    self.actor.load_state_dict(torch.load(filename + "_actor"))
+	    self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
+	    self.actor_target = copy.deepcopy(self.actor)
